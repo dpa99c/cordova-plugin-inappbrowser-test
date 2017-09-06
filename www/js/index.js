9d35066201f8d72863804c90d7742e44886de91a
Dave Alden <dpa99c@gmail.com>
2017-09-06 10:26:02 +0100

    Set an interval to repeatedly inject script into IAB

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,6 @@
 var URL = "iab_content_page.html";
 
-var webView, iabOpts, osVersion, iab;
+var webView, iabOpts, osVersion, iab, updateTimerId;
 
 function log(msg){
     console.log(msg);
@@ -11,31 +11,67 @@ function openIAB(){
     iab = cordova.InAppBrowser.open(URL, '_blank', iabOpts);
 
     iab.addEventListener('loadstart', function(e) {
-        log("received 'loadstart' for URL: "+ e.url);
+        log("received 'loadstart' event for: "+ e.url);
     });
     iab.addEventListener('loadstop', function(e) {
-        log("received 'loadstop' for URL: "+ e.url);
+        log("received 'loadstop' event for: "+ e.url);
         testInjection();
     });
     iab.addEventListener('loaderror', function(e) {
-        log("received 'loaderror' for URL: "+ e.url);
+        log("received 'loaderror' event for: "+ e.url);
+    });
+    iab.addEventListener('exit', function() {
+        log("received 'exit' event");
+        clearInterval(updateTimerId);
+        updateTimerId = null;
     });
 }
 
 function testInjection(){
     iab.executeScript({
-        code: "(function() { var body = document.querySelector('body'); var bottom = document.createElement('div'); bottom.innerHTML = 'Absolute Bottom'; bottom.classList.add('bottom');  body.appendChild(bottom); })(); document.getElementsByTagName('h1')[0].innerHTML = \" Injected Title\";"
+        code: "(function() { " +
+            "var body = document.querySelector('body'); " +
+            "var bottom = document.createElement('div'); " +
+            "bottom.innerHTML = 'Absolute Bottom'; " +
+            "bottom.classList.add('bottom');  " +
+            "body.appendChild(bottom); " +
+            "var time = document.createElement('div'); " +
+            "time.id=\"time\"" +
+            "body.appendChild(time); " +
+        "})(); " +
+        "document.getElementsByTagName('h1')[0].innerHTML = \" Injected Title\";"
     }, function(returnValue){
         returnValue = returnValue[0];
        log("executeScript callback returned : " + returnValue);
     });
 
     iab.insertCSS({
-        code: "body *{color: red !important;}\
-                  .bottom { position: fixed; bottom: 0; z-index: 500; width: 100%; background: black; opacity: 0.5; padding: 10px; font-size: 20px;}"
+        code: "body *{color: red !important;},"+
+              ".bottom { position: fixed; bottom: 0; z-index: 500; width: 100%; background: black; opacity: 0.5; padding: 10px; font-size: 20px;}"
     }, function(){
         log("insertCSS called back");
     });
+
+    updateTimerId = setInterval(function(){
+        iab.executeScript({
+            code: "(function() { " +
+                "var timeEl = document.getElementById('time');" +
+                "if(!timeEl){ " +
+                    "var body = document.querySelector('body'); " +
+                    "timeEl = document.createElement('div'); " +
+                    "timeEl.id=\"time\"; " +
+                    "body.appendChild(timeEl); " +
+                "}" +
+                "var time = (new Date()).toISOString(); " +
+                "timeEl.innerHTML = time; " +
+                "return time;" +
+            "})();"
+        }, function(returnValue){
+            returnValue = returnValue[0];
+            log("executeScript for updateTimer returned : " + returnValue);
+        });
+    }, 1000);
+
 }
 
 
